973aa382082656d67ad9c13b62d6be6171790eba
iminto <waitfox@qq.com>
2019-04-20 14:45:22 +0800

    依赖版本升级下

--- spx-web/src/main/java/spx/baicai/controller/UserController.java
+++ spx-web/src/main/java/spx/baicai/controller/UserController.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import spx.baicai.service.UserService;
 import spx.baicai.model.User;
+import spx.baicai.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")
