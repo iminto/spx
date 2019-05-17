efe0250b2426bb6447fbbf059bd3c32fae74d3da
iminto <waitfox@qq.com>
2019-05-17 23:29:23 +0800

    集成sentinel，待完善

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 [![build](https://www.travis-ci.org/iminto/spx.svg?branch=master)](https://www.travis-ci.org/iminto/spx)
 
+### 启动
+cd /soft/nacos/bin/ && sh startup.sh -m standalone
+java -Dserver.port=8858 -Dcsp.sentinel.dashboard.server=localhost:8858 -Dproject.name=sentinel-dashboard  -Dcsp.sentinel.api.port=8719  -jar sentinel-dashboard-1.6.0.jar
+java -Djava.net.preferIPv4Stack=true -Dcsp.sentinel.api.port=8719 -Dcsp.sentinel.dashboard.server=localhost:8858 -Dproject.name=spx  -jar spx-web-0.1.jar
 
-#### 启动
+#### Docker
 ```shell
 docker run -p 8080:8080 -t springboot/spx
 docker run -v  /tmp/springx:/tmp -p 8080:8080 -d  springboot/spx
--- spx-web/src/main/java/spx/baicai/handle/WebConfig.java
+++ spx-web/src/main/java/spx/baicai/handle/WebConfig.java
@@ -1,5 +1,8 @@
 package spx.baicai.handle;
 
+import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,6 +19,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+    }
+
+    @Bean
+    public FilterRegistrationBean sentinelFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new CommonFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("sentinelFilter");
+        registration.setOrder(1);
+        return registration;
     }
 
 }
